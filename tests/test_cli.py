9390aceda2696fe0db77a7e8eb0a7a50531ad45f
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-04-21 22:01:12 -0400

    Create and use CaptureStderr

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,7 @@
 
 
 # Python standard library
+import io
 import pathlib
 import subprocess
 import sys
@@ -12,6 +13,20 @@ import pytest
 
 # Tested package
 import sql_formatter
+
+
+class CaptureStderr(list):
+    """A context manager for capturing stderr"""
+
+    def __enter__(self):
+        self._stderr = sys.stderr
+        sys.stderr = self._stringio = io.StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio
+        sys.stderr = self._stderr
 
 
 testdata = [
@@ -33,6 +48,47 @@ def test_check_filename_extension_pass(capsys, filename):
     assert captured.err == ""
 
 
+testdata = [
+    (
+        "filename.txt",
+        "\x1b[1;31mInvalid file type. `filename.txt` is not a `.sql` file.\x1b[0m",
+    ),
+    (
+        "query.sql_2",
+        "\x1b[1;31mInvalid file type. `query.sql_2` is not a `.sql` file.\x1b[0m",
+    ),
+    (
+        "badfile.py",
+        "\x1b[1;31mInvalid file type. `badfile.py` is not a `.sql` file.\x1b[0m",
+    ),
+    (
+        "$ecretpassword123",
+        "\x1b[1;31mInvalid file type. `$ecretpassword123` is not a `.sql` file.\x1b[0m",
+    ),
+    (".", "\x1b[1;31mInvalid file type. `.` is not a `.sql` file.\x1b[0m"),
+    ("..", "\x1b[1;31mInvalid file type. `..` is not a `.sql` file.\x1b[0m"),
+]
+
+
+@pytest.mark.parametrize("filename,expected_stderr", testdata)
+def test_check_filename_extension_fail_new(capsys, filename, expected_stderr):
+    filename = "filename.txt"
+    expected_stderr = (
+        "\x1b[1;31mInvalid file type. `filename.txt` is not a `.sql` file.\x1b[0m"
+    )
+
+    filename = pathlib.Path(filename)
+
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        with CaptureStderr() as output:
+            sql_formatter.check_filename_extension(filename)
+
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
+    assert len(output) == 1
+    assert output[0] == expected_stderr
+
+
 #########################################################################################################
 #########################################################################################################
 
@@ -52,32 +108,38 @@ def test_check_filename_extension_pass(capsys, filename):
 #     diff = a - b
 #     assert diff == expected
 
+# def test_exit(mymodule):
+#     with pytest.raises(SystemExit) as pytest_wrapped_e:
+#             mymodule.will_exit_somewhere_down_the_stack()
+#     assert pytest_wrapped_e.type == SystemExit
+#     assert pytest_wrapped_e.value.code == 42
 
-def test_check_filename_extension_fail(capsys):
-    filename = "filename.txt"
-    # expected_error_message = "Invalid file type. `filename.txt` is not a `.sql` file."
-    expected_error_message = (
-        "\x1b[1;31mInvalid file type. `filename.txt` is not a `.sql` file.\x1b[0m"
-    )
 
-    filename = pathlib.Path(filename)
+# def test_check_filename_extension_fail(capsys):
+#     filename = "filename.txt"
+#     # expected_error_message = "Invalid file type. `filename.txt` is not a `.sql` file."
+#     expected_error_message = (
+#         "\x1b[1;31mInvalid file type. `filename.txt` is not a `.sql` file.\x1b[0m"
+#     )
 
-    with pytest.raises(SystemExit):
-        sql_formatter.check_filename_extension(filename)
+#     filename = pathlib.Path(filename)
 
-    # with pytest.raises(SystemExit):
-    #     # with pytest.raises(SystemExit, match = expected_error_message):
-    #     sql_formatter.check_filename_extension(filename)
+#     with pytest.raises(SystemExit):
+#         sql_formatter.check_filename_extension(filename)
 
-    # captured = capsys.readouterr()
-    # actual_error_message = captured.err
+# with pytest.raises(SystemExit):
+#     # with pytest.raises(SystemExit, match = expected_error_message):
+#     sql_formatter.check_filename_extension(filename)
 
-    # assert actual_error_message == expected_error_message
+# captured = capsys.readouterr()
+# actual_error_message = captured.err
 
-    # assert captured.err == "Invalid file type. `false.txt` is not a `.sql` file."
+# assert actual_error_message == expected_error_message
 
-    # print(captured.out)
-    # print(captured.err)
+# assert captured.err == "Invalid file type. `false.txt` is not a `.sql` file."
+
+# print(captured.out)
+# print(captured.err)
 
 
 # if not filename.suffix.lower() == ".sql":
