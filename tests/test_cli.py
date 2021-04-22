004ac49e4d5aa682187144d1787028ca99f156b0
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-04-21 22:03:41 -0400

    Remove old comments, add new comments

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -29,6 +29,7 @@ class CaptureStderr(list):
         sys.stderr = self._stderr
 
 
+# Cases for test_check_filename_extension_pass
 testdata = [
     ("filename.sql"),
     ("filename.SQL"),
@@ -48,6 +49,7 @@ def test_check_filename_extension_pass(capsys, filename):
     assert captured.err == ""
 
 
+# Cases for test_check_filename_extension_fail
 testdata = [
     (
         "filename.txt",
@@ -71,7 +73,7 @@ testdata = [
 
 
 @pytest.mark.parametrize("filename,expected_stderr", testdata)
-def test_check_filename_extension_fail_new(capsys, filename, expected_stderr):
+def test_check_filename_extension_fail(capsys, filename, expected_stderr):
     filename = "filename.txt"
     expected_stderr = (
         "\x1b[1;31mInvalid file type. `filename.txt` is not a `.sql` file.\x1b[0m"
@@ -87,73 +89,3 @@ def test_check_filename_extension_fail_new(capsys, filename, expected_stderr):
     assert pytest_wrapped_e.value.code == 1
     assert len(output) == 1
     assert output[0] == expected_stderr
-
-
-#########################################################################################################
-#########################################################################################################
-
-# 1. Write one test
-# 2. parameterize one test
-# 3. make more test cases
-
-
-# testdata = [
-#     (datetime(2001, 12, 12), datetime(2001, 12, 11), timedelta(1)),
-#     (datetime(2001, 12, 11), datetime(2001, 12, 12), timedelta(-1)),
-# ]
-
-
-# @pytest.mark.parametrize("a,b,expected", testdata)
-# def test_timedistance_v0(a, b, expected):
-#     diff = a - b
-#     assert diff == expected
-
-# def test_exit(mymodule):
-#     with pytest.raises(SystemExit) as pytest_wrapped_e:
-#             mymodule.will_exit_somewhere_down_the_stack()
-#     assert pytest_wrapped_e.type == SystemExit
-#     assert pytest_wrapped_e.value.code == 42
-
-
-# def test_check_filename_extension_fail(capsys):
-#     filename = "filename.txt"
-#     # expected_error_message = "Invalid file type. `filename.txt` is not a `.sql` file."
-#     expected_error_message = (
-#         "\x1b[1;31mInvalid file type. `filename.txt` is not a `.sql` file.\x1b[0m"
-#     )
-
-#     filename = pathlib.Path(filename)
-
-#     with pytest.raises(SystemExit):
-#         sql_formatter.check_filename_extension(filename)
-
-# with pytest.raises(SystemExit):
-#     # with pytest.raises(SystemExit, match = expected_error_message):
-#     sql_formatter.check_filename_extension(filename)
-
-# captured = capsys.readouterr()
-# actual_error_message = captured.err
-
-# assert actual_error_message == expected_error_message
-
-# assert captured.err == "Invalid file type. `false.txt` is not a `.sql` file."
-
-# print(captured.out)
-# print(captured.err)
-
-
-# if not filename.suffix.lower() == ".sql":
-#     message = f"Invalid file type. `{filename}` is not a `.sql` file."
-#     print_error(message)
-#     exit(1)
-
-
-# def test_print_error(capsys):
-#     print("hello")
-#     sys.stderr.write("world\n")
-#     captured = capsys.readouterr()
-#     assert captured.out == "hello\n"
-#     assert captured.err == "world\n"
-#     print("next")
-#     captured = capsys.readouterr()
-#     assert captured.out == "next\n"
