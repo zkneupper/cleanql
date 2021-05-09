35ce1c0cde7293947de59d66858d787bdd8afb34
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-05-09 19:54:27 -0400

    Add test_cli_help

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""Tests for `cleanql` package."""
+"""Tests for `cleanql` cli."""
 
 import pytest
-
 from click.testing import CliRunner
-
-# from cleanql import cleanql
-# from cleanql import cli
 import cleanql
 
 
 def test_cli_no_arguments():
     """Test the `cleanql` CLI with no arguments or options passed."""
-
     expected_result = "No Path provided. Nothing to do 😴\n"
-
     runner = CliRunner()
     result = runner.invoke(cleanql.cli)
-
     assert result.exit_code == 0
-    # assert "cleanql.cli" in result.output
     assert result.output == expected_result
 
-    # # assert "cleanql.cli.main" in result.output
-    # assert "cleanql.cli" in result.output
-    # help_result = runner.invoke(cli.main, ["--help"])
-    # assert help_result.exit_code == 0
-    # assert "--help  Show this message and exit." in help_result.output
 
+@pytest.mark.parametrize("help_flag", ["-h", "--help"])
+def test_cli_help(help_flag):
+    """Test the `cleanql` help doesn't error."""
+    expected_substrings = [
+        "The uncompromising SQL formatter.",
+        "Pass path(s) to SQL file(s) or folder(s) that contain(s) SQL files.",
+        "-f, --flavor [COMMON|HQL|ORACLE|POSTGRESQL]",
+        "-v, --verbose",
+    ]
+
+    runner = CliRunner()
+    result = runner.invoke(cleanql.cli, [help_flag])
+    assert result.exit_code == 0
+    for substring in expected_substrings:
+        assert substring in result.output
+
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
 
 # def test_command_line_interface():
 #     """Test the CLI."""
