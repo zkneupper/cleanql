292a29f4a5dc723050993caa7e3586bc364a01bd
Zachary Kneupper <zacharykneupper@Zacharys-MBP.lan>
2021-05-09 19:36:38 -0400

    Add test_cli_no_arguments

--- tests/test_cleanql.py
+++ tests/test_cleanql.py
@@ -1,40 +1,40 @@
-#!/usr/bin/env python
+# #!/usr/bin/env python
 
-"""Tests for `cleanql` package."""
+# """Tests for `cleanql` package."""
 
-import pytest
+# import pytest
 
-from click.testing import CliRunner
+# from click.testing import CliRunner
 
-# from cleanql import cleanql
-# from cleanql import cli
-import cleanql
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+# # from cleanql import cleanql
+# # from cleanql import cli
+# import cleanql
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
+
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     # import requests
+#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    # result = runner.invoke(cli.main)
-    result = runner.invoke(cleanql.cli)
-    assert result.exit_code == 0
-    # assert "cleanql.cli.main" in result.output
-    assert "cleanql.cli" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
-    assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+# def test_content(response):
+#     """Sample pytest test function with the pytest fixture as an argument."""
+#     # from bs4 import BeautifulSoup
+#     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
+# def test_command_line_interface():
+#     """Test the CLI."""
+#     runner = CliRunner()
+#     # result = runner.invoke(cli.main)
+#     result = runner.invoke(cleanql.cli)
+#     assert result.exit_code == 0
+#     # assert "cleanql.cli.main" in result.output
+#     assert "cleanql.cli" in result.output
+#     help_result = runner.invoke(cli.main, ["--help"])
+#     assert help_result.exit_code == 0
+#     assert "--help  Show this message and exit." in help_result.output
